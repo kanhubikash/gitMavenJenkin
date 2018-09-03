51207b5e928a47c0c739c50a73dd2a1598a7ca97
kanhubikash <kanhubikash@gmail.com>
2018-09-03 13:32:32 +0530

    commiting first project

---
+++ TestGitMavenJenkin1/src/main/java/com/git/maven/jenkin/TestGitMavenJenkin1/App.java
@@ -0,0 +1,13 @@
+package com.git.maven.jenkin.TestGitMavenJenkin1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
